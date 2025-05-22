f9ca48ed6b675021bc98d1d49ba164c0cf2e83ff
neiranie <nurkhairanianneira@gmail.com>
2025-05-22 10:16:59 +0700

    Returning the value of the top node

--- StackUsingLinkedList.cpp
+++ StackUsingLinkedList.cpp
@@ -52,3 +52,22 @@ public:
     }
 
     // Peek/Top operation: Retrieve the value of the topmost element without removing
+    void peek()
+    {
+        if (top == NULL)
+        {
+            cout << "List is empty." << endl;
+        }
+        else
+        {
+            Node *current = top;
+            while (current != NULL)
+            {
+                cout << current->data << " " endl;
+                current = current->next;
+            }
+            cout << endl;
+        } // Return the value of the top node
+    }
+
+    
